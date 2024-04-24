07759dc8f0f97a74068c2a97a83584ef87163d63
Daniel-Amadeus <dajg1995@gmail.com>
2024-04-24 10:13:32 +0200

    added cmake, main and gtest

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "PixelSynC++\n";
+    return 0;
+}
---
+++ unitTests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(ExampleTest, Test_0)
+{
+    EXPECT_EQ(0, 0);
+}
